345fe3e8a5dd51675137208d2446713d76cd3577
Harshil <harshil.chhajed12@gmail.com>
2016-04-24 14:22:35 +0530

    Assignment 1
    Session 3 Assignment 1

---
+++ CalRoot.java
@@ -0,0 +1,14 @@
+package acadgild;
+import java.math.*;
+
+public class CalRoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 4;
+		int b=27;
+		System.out.println("Square root of 4 is  ::  "+ Math.sqrt(a));
+		System.out.println("Square root of 4 is  ::  "+ Math.cbrt(b));
+	}
+
+}
